88569830ad9da6b7817031632282e4d6d7059f8e
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2025-08-06 21:59:42 +0200

    feat: adds ReviewCard Components Break: Moves SectionalHeader to Components

---
+++ Revive/src/Components/ReviewCard.jsx
@@ -0,0 +1,17 @@
+function ReviewCard(props) {
+
+    return(
+        <div className="review-card">
+            <img src={reviewImg} alt="" />
+            <h1>{reviewName}</h1>
+        </div>
+    )
+}
+
+MyComponent.propTypes = {
+    reviewImg: PropTypes.src.isRequired,
+    reviewName: PropTypes.string.isRequired,
+};
+
+
+export default ReviewCard
